8a0c5afbb3e7064b9446f24f241305042d0a178c
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-02-03 15:27:09 +0530

    added icons to card

--- src/components/PDFCard.tsx
+++ src/components/PDFCard.tsx
@@ -1,11 +1,19 @@
 import pdf from "../assets/pdf-svgrepo-com.svg"
 import { Button } from "./Button"
 import ChatIcon from "./icons/ChatIcon"
+import DeleteIcon from "./icons/DeleteIcon"
+import FavoritesIcon from "./icons/FavourtieIcon"
 export const PDFCard = ({pdfName}:{
     pdfName:string
 }) =>{
     return(
         <div className="w-80  border border-gray-200/20 rounded-lg">
+            <div className="flex justify-end">
+                <div className="flex gap-2 p-2 absolute">
+                    <FavoritesIcon/>
+                    <DeleteIcon/>
+                </div>
+            </div>
         <div className="h-40 border-b border-gray-200/20 flex justify-center items-center">
             <img src={pdf} alt="PDF icon" className="w-[80%] h-[80%]" />
         </div>
