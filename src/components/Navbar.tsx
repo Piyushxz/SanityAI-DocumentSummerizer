d32727a61aa87655a2ed50f3f70fa55d5e01677d
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-02-09 16:11:53 +0530

    Save changes before merge

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = ()=>{
         <motion.div 
         initial={{opacity:0,y:-20}}
         animate={{opacity:1,y:0}}
-        transition={{delay:0.2,ease:"easeInOut"}}
+        transition={{delay:0.3,duration:0.4,ease:"easeInOut"}}
         className="w-[100vw] flex justify-center border-b border-gray-400/50">
             <div className=" w-[80vw] flex justify-between p-4  ">
                 <h1 className="font-primary font-extrabold text-[#FDFEFF] text-3xl text-primary tracking-tighter  bg-gradient-to-b from-blue-400 to-blue-700 bg-clip-text  text-transparent">sanityAI</h1>
