91cb1251be7a0db089a6f8340c5d218fbbbdb5e7
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-02-14 01:27:29 +0530

    added functionality to add to archived

--- src/components/PDFCard.tsx
+++ src/components/PDFCard.tsx
@@ -6,6 +6,8 @@ import DeleteIcon from "./icons/DeleteIcon"
 import FavoritesIcon from "./icons/FavourtieIcon"
 import { useSetRecoilState } from "recoil"
 import { activeDocumentData, activeSidebarOption, isDeleteModalOpen } from "../atoms"
+import { toast } from "sonner"
+import axios from "axios"
 
 export const PDFCard = ({pdfName,documentId,isArchived}:{
     pdfName:string,
@@ -13,17 +15,46 @@ export const PDFCard = ({pdfName,documentId,isArchived}:{
     isArchived:boolean
 }) =>{
 
+
     const navigate = useNavigate()
     const setSideBarOption = useSetRecoilState(activeSidebarOption)
     const setActiveDocData = useSetRecoilState(activeDocumentData)
     const setDelModal = useSetRecoilState(isDeleteModalOpen)
 
+    const handleAddToFavouriteClick = async () => {
+        try {
+            console.log(documentId);
+            
+            const response = await toast.promise(
+                 axios.post('http://localhost:3003/api/v1/favourite', {
+                    document: documentId},
+                    {
+                        headers:{
+                            Authorization:localStorage.getItem("token")
+                        }
+                    
+                }),
+                {
+                    loading: isArchived ?`Removing ${pdfName} from your favourites`: `Adding ${pdfName} to your favourites`,
+                    success:isArchived ?`Removed ${pdfName} from your favourites`: `Added ${pdfName} to your favourites`,
+                    error: isArchived ? `Could not remove ${pdfName} from your favourites`:`Could not add ${pdfName} to your favourites`
+                }
+            );
+    
+            console.log(response)
+        } catch (err) {
+            console.log(err);
+        }
+    };
     
     return(
         <div className="w-80  border border-gray-200/20 rounded-lg">
             <div className="flex justify-end">
                 <div className="flex gap-2 p-2 absolute">
+                    <div onClick={handleAddToFavouriteClick}>
                     <FavoritesIcon isArhived={isArchived}/>
+
+                    </div>
                     <div onClick={()=>{setDelModal(true)
                         setActiveDocData({documentId:documentId,documentName:pdfName})
                     }}>
--- src/components/icons/FavourtieIcon.tsx
+++ src/components/icons/FavourtieIcon.tsx
@@ -1,6 +1,6 @@
-const FavoritesIcon = ({ className = "", size = 24, color = "currentColor",isArhived = false }) => {
+const FavoritesIcon = ({ className = "", size = 24, color = "currentColor",isArhived = false, }) => {
     return (
-      <svg
+      <svg 
         xmlns="http://www.w3.org/2000/svg"
         width={size}
         height={size}
