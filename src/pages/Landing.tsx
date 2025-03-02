74f44cef438a4b26e41c38251c24265c5d1f00a6
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-03-02 20:23:52 +0530

    cool stuff

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -10,6 +10,10 @@ import UploadFolderIcon from "../components/icons/UploadFolder";
 import MessagesIcon from "../components/icons/MessagesIcon";
 import { MailIcon, Newspaper, Rocket, Sparkles, VideoIcon } from "lucide-react";
 import { Footer } from "@/components/Footer";
+import ss from "../assets/ss.png"
+import stars from "../assets/stars.png"
+import gridline from "../assets/grid-lines.png"
+
 
 export const Landing = () => {
   const navigate = useNavigate()
@@ -64,9 +68,9 @@ export const Landing = () => {
                  initial={{ opacity: 0, y: 0 }}
                  animate={{ opacity: 1, y: 20 }}
          transition={{ type: "spring", stiffness: 200,bounce:0.1, duration:0.4,delay:0.5,damping:15}}
-        className="px-4 rounded-full   bg-blue-600/20 flex gap-2 mb-5 py-2 max-w-fit flex items-center border border-blue-600/30 ">
-              <Rocket className="  size-6 text-white" />
-              <h1 className="text-md  font-primary tracking-tighter font-medium text-white">sanityAI beta ready to use.</h1>
+        className="px-4 rounded-full  bg-blue-600/20  flex gap-2 mb-5 py-2 max-w-fit flex items-center border border-blue-600/30 ">
+              <Rocket className="  size-4 text-blue-500/70" />
+              <h1 className="text-sm  font-primary tracking-tighter font-medium text-blue-500/70">sanityAI beta ready to use.</h1>
             </motion.div>
           <motion.div
                     initial={{ opacity: 0, y: 0 }}
@@ -144,11 +148,108 @@ export const Landing = () => {
             </h1>
           </motion.div>
           <Features />
+            <div className="w-full flex justify-center">
+            <div className="w-[90vw] md:w-[80vw] lg:w-[70vw] border border-white/20 p-2.5 rounded-xl mt-3">
+                <div style={{backgroundImage :`url(${ss})`}}
+                className="aspect-video bg-cover border border-white/20 rounded-lg">
 
-          <div className="w-full flex justify-center my-[400px] ">
+                </div>
+            </div>
+            </div>
+
+            <div className="w-full flex justify-center mt-[300px] ">
           
-              <h1 className="font-primary tracking-tighter font-extrabold text-3xl md:text-5xl text-white">Summarize.Chat.And More</h1>
             
+          <div className="h-[70vh] md:h-[50vh] lg:h-[70vh]  bg-[#191919] w-1 rounded-lg">
+           <motion.span
+          ref = {targetRef}
+           style={{y:pos
+
+
+           }}
+          
+          className="h-30  w-1 mb-[500px] absolute rounded-lg bg-gradient-to-b from-blue-400 to-blue-700  rounded-full drop-shadow-xl shadow-[0_0_30px_3px_rgba(0,123,255,0.8)]">
+
+            
+          </motion.span> 
+
+
+          <div className="">
+          <motion.div
+          ref={iconRef}
+          style={{ color: col }}
+          className="absolute mb-6  -translate-x-38">
+            <SearchFolderIcon className="text-inherit size-28" />
+          </motion.div>
+
+          <div className="w-40 h-24 mt-8 translate-x-4 ">
+            <span className="rounded-full font-primary bg-[#3B82F6] px-4 py-2 text-white ">1</span>
+            <h1 className="font-primary tracking-tighter font-normal text-xl text-white px-2 mt-4">Select a PDF</h1>
+            <h1 className="font-primary tracking-tighter font-normal text-md text-white/50 px-2">Select a PDF you want to chat with.</h1>
+
+          </div>
+
+          </div>
+          <div className="translate-y-20">
+          <motion.div
+          ref={iconRef2}
+          style={{ color: col2 }}
+          className="absolute mb-6  translate-x-8">
+            <UploadFolderIcon className="text-inherit size-28" />
+          </motion.div>
+
+          <div className="w-40 h-24 mt-8 -translate-x-38 ">
+            <span className="rounded-full font-primary bg-[#3B82F6] px-4 py-2 text-white ">2</span>
+            <h1 className="font-primary tracking-tighter font-normal text-xl text-white px-2 mt-4">Upload </h1>
+            <h1 className="font-primary tracking-tighter font-normal text-md text-white/50 px-2">Upload your PDF with one click.</h1>
+
+          </div>
+
+          </div>
+          <div className="translate-y-36">
+          <motion.div
+          ref={iconRef3}
+          style={{ color: col3 }}
+          className="absolute mb-6  -translate-x-38">
+            <MessagesIcon className="text-inherit size-28" />
+          </motion.div>
+
+          <div className="w-40 h-24 mt-8 translate-x-4 ">
+            <span className="rounded-full font-primary bg-[#3B82F6] px-4 py-2 text-white ">3</span>
+            <h1 className="font-primary tracking-tighter font-normal text-xl text-white px-2 mt-4">Chat</h1>
+            <h1 className="font-primary tracking-tighter font-normal text-md text-white/50 px-2">Ask any question you want.</h1>
+
+          </div>
+
+          </div>
+          
+          </div>
+
+
+      </div>
+          <div className="w-full flex justify-center my-[200px] ">
+              <section className="py-20  w-[90vw]  md:w-[70vw]">
+                <div style={{backgroundImage:`url(${stars})`}}
+                 className="border border-white/15 py-24 rounded-xl overflow-hidden relative">
+                    <div style={{backgroundImage:`url(${gridline})`}} className="absolute inset-0 bg-[rgba(0,48,135,1)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at+50%_35%,black,transparent)] ">                    </div>
+                    <div className="relative">
+                    <h2 className="text-5xl md:text-6xl max-w-sm mx-auto tracking-tight text-center text-white font-primary font-medium">
+                Document, Summarize, and More
+              </h2>                        <p className="text-center text-lg md:text-xl  max-w-xs mx-auto text-white/70 px-4 mt-5 tracking-tighter font-primary ">Achieve clear , impactful results without the complexity.</p>
+                        <div className="flex justify-center mt-8">
+                        <motion.button
+                            whileTap={{ scale: 0.95 }}
+                            onClick={() => navigate('/login')}
+                            className="inline-flex font-primary tracking-tighter items-center justify-center bg-gradient-to-b from-blue-400 to-blue-700 text-white font-normal hover:opacity-80 transition-all duration-300 h-11 rounded-md px-6 md:px-10"
+                          >
+                            Start Now
+                          </motion.button>
+                        </div>
+                    </div>
+
+                </div>
+              </section>
+
 
           </div>
 
@@ -187,76 +288,7 @@ export const Landing = () => {
 </div>
 
 
-          <div className="w-full flex justify-center mt-[300px] ">
-          
-            
-            <div className="h-[70vh] md:h-[50vh] lg:h-[70vh]  bg-[#191919] w-1 rounded-lg">
-             <motion.span
-            ref = {targetRef}
-             style={{y:pos
 
-
-             }}
-            
-            className="h-30  w-1 mb-[500px] absolute rounded-lg bg-gradient-to-b from-blue-400 to-blue-700  rounded-full drop-shadow-xl shadow-[0_0_30px_3px_rgba(0,123,255,0.8)]">
-
-              
-            </motion.span> 
-
-
-            <div className="">
-            <motion.div
-            ref={iconRef}
-            style={{ color: col }}
-            className="absolute mb-6  -translate-x-38">
-              <SearchFolderIcon className="text-inherit size-28" />
-            </motion.div>
-
-            <div className="w-40 h-24 mt-8 translate-x-4 ">
-              <span className="rounded-full font-primary bg-[#3B82F6] px-4 py-2 text-white ">1</span>
-              <h1 className="font-primary tracking-tighter font-normal text-xl text-white px-2 mt-4">Select a PDF</h1>
-              <h1 className="font-primary tracking-tighter font-normal text-md text-white/50 px-2">Select a PDF you want to chat with.</h1>
-
-            </div>
-
-            </div>
-            <div className="translate-y-20">
-            <motion.div
-            ref={iconRef2}
-            style={{ color: col2 }}
-            className="absolute mb-6  translate-x-8">
-              <UploadFolderIcon className="text-inherit size-28" />
-            </motion.div>
-
-            <div className="w-40 h-24 mt-8 -translate-x-38 ">
-              <span className="rounded-full font-primary bg-[#3B82F6] px-4 py-2 text-white ">2</span>
-              <h1 className="font-primary tracking-tighter font-normal text-xl text-white px-2 mt-4">Upload </h1>
-              <h1 className="font-primary tracking-tighter font-normal text-md text-white/50 px-2">Upload your PDF with one click.</h1>
-
-            </div>
-
-            </div>
-            <div className="translate-y-36">
-            <motion.div
-            ref={iconRef3}
-            style={{ color: col3 }}
-            className="absolute mb-6  -translate-x-38">
-              <MessagesIcon className="text-inherit size-28" />
-            </motion.div>
-
-            <div className="w-40 h-24 mt-8 translate-x-4 ">
-              <span className="rounded-full font-primary bg-[#3B82F6] px-4 py-2 text-white ">3</span>
-              <h1 className="font-primary tracking-tighter font-normal text-xl text-white px-2 mt-4">Chat</h1>
-              <h1 className="font-primary tracking-tighter font-normal text-md text-white/50 px-2">Ask any question you want.</h1>
-
-            </div>
-
-            </div>
-            
-            </div>
- 
-
-        </div>
           <div id="about-us ">
             <Aboutus />
           </div>
